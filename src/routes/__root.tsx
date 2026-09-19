30e74ac55bb9fdbba8b4c26e90ef5cab0b7eb326
salimpk743-droid <salimpk743@gmail.com>
2026-09-19 12:57:37 +0500

    Use production PWA manifest for Android TWA

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,7 +28,7 @@ export const Route = createRootRoute({
     links: [
       { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "stylesheet", href: appCss },
-      { rel: "manifest", href: "/__grok/manifest.webmanifest" },
+      { rel: "manifest", href: "/manifest.webmanifest" },
       { rel: "apple-touch-icon", href: "/__grok/icon-180.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
